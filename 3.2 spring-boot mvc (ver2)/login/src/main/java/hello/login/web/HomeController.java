588cf2d2b36d786a959e0b2c0cda63f4b1b6764d
Kohaneul <happyhn_@naver.com>
2022-08-11 19:14:45 +0900

    0811 commit

--- 3.2 spring-boot mvc (ver2)/login/src/main/java/hello/login/web/HomeController.java
+++ 3.2 spring-boot mvc (ver2)/login/src/main/java/hello/login/web/HomeController.java
@@ -3,14 +3,17 @@ package hello.login.web;
 import hello.login.domain.login.LoginService;
 import hello.login.domain.member.Member;
 import hello.login.domain.member.MemberRepository;
+import hello.login.web.session.SessionManager;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import java.util.UUID;
 
 @Slf4j
@@ -19,25 +22,67 @@ import java.util.UUID;
 public class HomeController {
 
     private final MemberRepository memberRepository;
-
+    private final SessionManager sessionManager;
    // @GetMapping("/")
     public String home() {
         return "home";
     }
 
-    @GetMapping("/")    //required=false : 로그인 안한 사용자도 들어와야하기 때문
-    public String loginHome(@CookieValue(name="memberId",required = false)Long memberId, Model model){
-        //memberId를 String 으로 보냈으나(LoginController login 메서드 참고), 스프링이 알아서 Long 타입으로 컨버팅 해줌.
-         if(memberId==null){
-             return "home";
-         }
-         //로그인 성공
-        Member loginMember = memberRepository.findById(memberId);   //DB에서 찾은 member가 없으면
-             if(loginMember==null){
-                 return "home"; //home으로 보낸다.
-             }
-             model.addAttribute("member",loginMember);
+  //  @GetMapping("/")    //required=false : 로그인 안한 사용자도 들어와야하기 때문
+//    public String loginHomeV1(@CookieValue(name="memberId",required = false)Long memberId, Model model){
+//        //memberId를 String 으로 보냈으나(LoginController login 메서드 참고), 스프링이 알아서 Long 타입으로 컨버팅 해줌.
+//         if(memberId==null){
+//             return "home";
+//         }
+//         //로그인 성공
+//        Member loginMember = memberRepository.findById(memberId);   //DB에서 찾은 member가 없으면
+//             if(loginMember==null){
+//                 return "home"; //home으로 보낸다.
+//             }
+//             model.addAttribute("member",loginMember);
+//
+//        return "/members/loginHome";
+//    }
 
+
+  //  @GetMapping("/")
+    public String loginHomeV2(HttpServletRequest request,Model model){
+        //세션 관리자에 저장된 회원 정보 조회
+        Member member = (Member)sessionManager.getSession(request);
+        if(member==null){
+            return "home";
+        }
+        model.addAttribute("member",member);
         return "/members/loginHome";
     }
+
+  //  @GetMapping("/")
+    public String loginHomeV3(HttpServletRequest request,Model model){
+        HttpSession session = request.getSession(false);    //세션을 생성하지 않으므로 false
+        //세션이 유지되면 로그인으로 이동
+        //세션에 회원데이터가 없으면 home
+        if(session==null){
+            return "home";
+        }
+        Member member=(Member)session.getAttribute(SessionConst.LOGIN_MEMBER);
+        if(member==null){
+            return "home";
+        }
+        model.addAttribute("member",member);
+
+        return "/members/loginHome";
+
+    }
+
+    @GetMapping("/")
+    public String loginHomeV3Spring(@SessionAttribute(name = SessionConst.LOGIN_MEMBER,required = false)Member loginMember,Model model){
+        if(loginMember==null){
+            return "home";
+        }
+        model.addAttribute("member",loginMember);
+        return "/members/loginHome";
+    }
+
+
+
 }
--- 3.2 spring-boot mvc (ver2)/login/src/main/java/hello/login/web/login/LoginController.java
+++ 3.2 spring-boot mvc (ver2)/login/src/main/java/hello/login/web/login/LoginController.java
@@ -3,8 +3,11 @@ package hello.login.web.login;
 import hello.login.domain.login.LoginService;
 import hello.login.domain.member.Member;
 import hello.login.domain.member.MemberRepository;
+import hello.login.web.SessionConst;
+import hello.login.web.session.SessionManager;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
@@ -13,10 +16,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.print.attribute.standard.PresentationDirection;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
+import java.text.CollationKey;
 import java.util.UUID;
 
 @Controller
@@ -24,6 +30,7 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public class LoginController {
     private final LoginService loginService;
+    private final SessionManager sessionManager;
 
     @GetMapping("/login")
     public String loginForm(@ModelAttribute("loginForm")LoginForm form){
@@ -32,7 +39,7 @@ public class LoginController {
 
 
 
-    @PostMapping("/login")
+   // @PostMapping("/login")
     public String login(@Valid @ModelAttribute LoginForm loginForm, BindingResult bindingResult, HttpServletResponse response){
         if(bindingResult.hasErrors()){
             return "login/loginForm";
@@ -48,22 +55,75 @@ public class LoginController {
 
         //쿠키에 시간 정보를 주지 않으면 세션 쿠키(브라우저 종료시 모두 종료)
 
-        Cookie cookie = new Cookie("memberId", String.valueOf(loginMember.getId()));
-        response.addCookie(cookie);
+      //  Cookie cookie = new Cookie("memberId", String.valueOf(loginMember.getId()));
+       // response.addCookie(cookie);
 
 
         return "redirect:/";
     }
+   // @PostMapping("/login")
+    public String loginV2(@Valid @ModelAttribute LoginForm loginForm, BindingResult bindingResult,HttpServletResponse response){
+        if(bindingResult.hasErrors()){
+            return "login/loginForm";
+        }
+        Member member = loginService.login(loginForm.getLoginId(), loginForm.getPassword());
+        if(member==null){
+            return "login/loginForm";
+        }
+        //로그인 성공 처리
+
+        //세션 관리자를 통해 세션을 생성하고 회원 데이터를 보관
+        sessionManager.createSession(member,response);
+        return "redirect:/";
+
+    }
+
+    @PostMapping("/login")
+    public String loginV3(@Valid @ModelAttribute LoginForm loginForm, BindingResult bindingResult, HttpServletRequest request){
+        if(bindingResult.hasErrors()){
+            return "login/loginForm";
+        }
+        Member member = loginService.login(loginForm.getLoginId(), loginForm.getPassword());
+        if(member==null){
+            return "login/loginForm";
+        }
+        //로그인 성공 처리
+        //세션이 있으면 있는 세션 반환, 없으면 신규 세션 생성
+        HttpSession session = request.getSession();//default : true
+        //true: (세션이 있으면 기존 세션 반환. 세션이 없으면 새로운 세션 생성해서 반환)
+        //false: 세션이 있으면 기존 세션 반환. 세션이 없으면 새로운 세션을 생성하지 않음. null 반환함.
+        //세션에 로그인 회원정보 보관
+        session.setAttribute(SessionConst.LOGIN_MEMBER,member);
+
+        return "redirect:/";
+
+    }
 
 
-    @PostMapping("/logout")
+    //@PostMapping("/logout")
     public String logout(HttpServletResponse response){
         expireCookie(response,"memberId");
         return "redirect:/";
     }
+  //  @PostMapping("/logout")
+    public String logoutV2(HttpServletRequest request){
+        sessionManager.expire(request);
+        return "redirect:/";
+    }
+    @PostMapping("/logout")
+    public String logoutV3(HttpServletRequest request){
+        HttpSession session = request.getSession(false);    //있는 세션을 가져오고
+        if(session!=null){
+            session.invalidate();   //세션+그 안에 데이터가 다 없어짐
+        }
+        return "redirect:/";
+    }
+
+
+
 
     private void expireCookie(HttpServletResponse response,String cookieName) {
-        Cookie cookie = new Cookie("memberId", null);
+        Cookie cookie = new Cookie("mySessionId", null);
         cookie.setMaxAge(0);
         response.addCookie(cookie);
     }
