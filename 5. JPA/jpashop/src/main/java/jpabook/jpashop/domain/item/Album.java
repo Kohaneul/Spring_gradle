4df6b2dfb3084c7c92db0a796dbef564c40f6e2a
Kohaneul <happyhn_@naver.com>
2022-09-25 15:57:59 +0900

    0925 commit

--- 5. JPA/jpashop/src/main/java/jpabook/jpashop/domain/item/Album.java
+++ 5. JPA/jpashop/src/main/java/jpabook/jpashop/domain/item/Album.java
@@ -1,12 +1,9 @@
 package jpabook.jpashop.domain.item;
 
-import jpabook.jpashop.domain.Category;
 import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
-import java.util.ArrayList;
-import java.util.List;
 
 
 @Getter @Setter
