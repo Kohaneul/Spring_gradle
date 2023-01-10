d1173bc82be1b94a65e740b7427bfe368ac7e70d
Kohaneul <happyhn_@naver.com>
2023-01-10 20:38:55 +0900

    0110 commit

--- 4.2 spring DB (ver2)/springtx/src/main/java/hello/springtx/propagation/MemberService.java
+++ 4.2 spring DB (ver2)/springtx/src/main/java/hello/springtx/propagation/MemberService.java
@@ -3,6 +3,8 @@ package hello.springtx.propagation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.UnexpectedRollbackException;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
--- 4.2 spring DB (ver2)/springtx/src/test/java/hello/springtx/propagation/MemberServiceTest.java
+++ 4.2 spring DB (ver2)/springtx/src/test/java/hello/springtx/propagation/MemberServiceTest.java
@@ -53,12 +53,11 @@ class MemberServiceTest {
 
         //given
         String userName = "로그예외_outerTxOff_success";
-
         //when
         assertThatThrownBy(()->memberService.joinV1(userName)).isInstanceOf(RuntimeException.class);
 
         //then : log 데이터는 롤백된다.
-        assertTrue(memberRepository.findByName(userName).isPresent());
+        assertTrue(memberRepository.findByName(userName).isEmpty());
         assertTrue(logRepository.findByMessage(userName).isEmpty());
     }
 
@@ -82,11 +81,11 @@ class MemberServiceTest {
         assertTrue(logRepository.findByMessage(userName).isPresent());
     }
 
-    @Test
-    void hello(){
-        org.assertj.core.api.Assertions.assertThatThrownBy(()->memberRepository.save1(new Member("hello"))).isInstanceOf(InvalidDataAccessApiUsageException.class);
-        assertTrue(memberRepository.findByName("hello").isEmpty());
-    }
+//    @Test
+//    void hello(){
+//        org.assertj.core.api.Assertions.assertThatThrownBy(()->memberRepository.save1(new Member("hello"))).isInstanceOf(InvalidDataAccessApiUsageException.class);
+//        assertTrue(memberRepository.findByName("hello").isEmpty());
+//    }
 
 
 
@@ -146,6 +145,28 @@ class MemberServiceTest {
 
         //then : 모든 데이터가 롤백된다.
         assertTrue(memberRepository.findByName(userName).isEmpty());
+        assertTrue(logRepository.findByMessage(userName).isEmpty());
+
+    }
+
+
+    /**
+     *
+     * memberService        @Transactional : ON
+     * logRepository        @Transactional : ON
+     *  memberRepository    @Transactional : ON(REQUIRES_NEW) Exception
+     *
+     * */
+    @Test
+    void recoverException_success(){
+        //given
+        String userName = "로그예외_recoverException_fail";
+        //when
+       memberService.joinV4(userName);
+
+
+        //then : member 저장, log 롤백.
+        assertTrue(memberRepository.findByName(userName).isPresent());
         assertTrue(logRepository.findByMessage(userName).isEmpty());
 
     }
