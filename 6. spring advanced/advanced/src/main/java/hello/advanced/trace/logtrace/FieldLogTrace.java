04b4b609f8d90c52f402288dd7c2f6a5343f59f4
Kohaneul <happyhn_@naver.com>
2023-01-23 19:11:11 +0900

    0123 commit

--- 6. spring advanced/advanced/src/main/java/hello/advanced/trace/logtrace/FieldLogTrace.java
+++ 6. spring advanced/advanced/src/main/java/hello/advanced/trace/logtrace/FieldLogTrace.java
@@ -75,3 +75,4 @@ public class FieldLogTrace implements LogTrace{
 
     }
 }
+
--- 6. spring advanced/advanced/src/test/java/hello/advanced/trace/logtrace/FieldLogTraceTest.java
+++ 6. spring advanced/advanced/src/test/java/hello/advanced/trace/logtrace/FieldLogTraceTest.java
@@ -1,11 +1,8 @@
 package hello.advanced.trace.logtrace;
 
 import hello.advanced.trace.TraceStatus;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 @SpringBootTest
 class FieldLogTraceTest {
