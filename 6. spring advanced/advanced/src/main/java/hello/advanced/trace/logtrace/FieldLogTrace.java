ac2a98bbc55c9a3a978f3e1b4cc00bcf4352ca26
Kohaneul <happyhn_@naver.com>
2023-01-23 18:36:36 +0900

    0123 commit

---
+++ 6. spring advanced/advanced/src/main/java/hello/advanced/trace/logtrace/FieldLogTrace.java
@@ -0,0 +1,77 @@
+package hello.advanced.trace.logtrace;
+
+import hello.advanced.trace.TraceId;
+import hello.advanced.trace.TraceStatus;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class FieldLogTrace implements LogTrace{
+    private static final String START_PREFIX = "-->";
+    private static final String COMPLETE_PREFIX = "<--";
+    private static final String EX_PREFIX = "<X--";
+
+    private TraceId traceIdHolder;  // ** traceId 동기화 , 동시성 이슈 발생
+
+    @Override
+    public TraceStatus begin(String message) {
+        syncTraceId();  //해당 메서드를 통해서 holder 에 값이 들어가있는게 보장됨.
+        long startTimeMs = System.currentTimeMillis();
+        //로그 출력
+        log.info("[{}] {}{}",traceIdHolder.getId(),addSpace(START_PREFIX,traceIdHolder.getLevel()),message);
+        return new TraceStatus(traceIdHolder,startTimeMs,message);
+    }
+
+    private void syncTraceId(){
+        if(traceIdHolder==null){    //최초호출이면 새로만든다.
+            traceIdHolder = new TraceId();
+        }
+        else{   //최초호출이 아니면 동기화
+            traceIdHolder = traceIdHolder.createNextId();
+        }
+
+    }
+
+    private String addSpace(String prefix,int level){
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i<level;i++){
+            sb.append((i==level-1)?"|"+prefix:"|  ");
+        }
+
+        return sb.toString();
+    }
+
+    @Override
+    public void end(TraceStatus traceStatus) {
+        complete(traceStatus,null);
+    }
+
+    private void complete(TraceStatus status, Exception e) {
+        Long stopTimeMs = System.currentTimeMillis();
+        long resultTimeMs = stopTimeMs - status.getStartTimeMs();
+        TraceId traceId = status.getTraceId();
+        if(e==null){
+            log.info("[{}] {}{} time={}ms",traceId.getId(),addSpace(COMPLETE_PREFIX,traceId.getLevel()),status.getMessage(),resultTimeMs);
+        }
+        else{
+            log.info("[{}] {}{} time={}ms ex={}",traceId.getId(),addSpace(EX_PREFIX,traceId.getLevel()),status.getMessage(),resultTimeMs,e.toString());
+        }
+        releaseTraceId();
+    }
+
+    private void releaseTraceId() {
+    if(traceIdHolder.isFirstLevel()){
+        traceIdHolder = null;   //destroy
+    }
+    else{   //중간단계일때
+        traceIdHolder = traceIdHolder.createPreviousId();
+    }
+
+    }
+
+
+    @Override
+    public void exception(TraceStatus status, Exception e) {
+        complete(status,e);
+
+    }
+}
---
+++ 6. spring advanced/advanced/src/test/java/hello/advanced/trace/logtrace/FieldLogTraceTest.java
@@ -0,0 +1,32 @@
+package hello.advanced.trace.logtrace;
+
+import hello.advanced.trace.TraceStatus;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+@SpringBootTest
+class FieldLogTraceTest {
+
+    FieldLogTrace trace = new FieldLogTrace();
+
+
+    @Test
+    void begin_end_level2(){
+        TraceStatus hello1 = trace.begin("hello1");
+        TraceStatus hello2 = trace.begin("hello2");
+        trace.end(hello2);
+        trace.end(hello1);
+    }
+    @Test
+    void begin_exception_level2(){
+        TraceStatus hello1 = trace.begin("hello1");
+        TraceStatus hello2 = trace.begin("hello2");
+        trace.exception(hello2,new IllegalStateException());
+        trace.exception(hello1,new IllegalStateException());
+
+    }
+
+}
