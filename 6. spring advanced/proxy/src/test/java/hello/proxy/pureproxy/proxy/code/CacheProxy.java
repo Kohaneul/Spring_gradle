6f153e90cb25f4bb035fc397dbdb9250e33bd24f
Kohaneul <happyhn_@naver.com>
2023-02-20 23:00:39 +0900

    0220 commit

---
+++ 6. spring advanced/proxy/src/test/java/hello/proxy/pureproxy/proxy/code/CacheProxy.java
@@ -0,0 +1,25 @@
+package hello.proxy.pureproxy.proxy.code;
+
+import lombok.extern.slf4j.Slf4j;
+
+
+@Slf4j
+public class CacheProxy implements Subject {
+
+    private Subject target;
+    private String cacheValue;
+
+    public CacheProxy(Subject target) {
+        this.target = target;
+    }
+
+    @Override
+    public String operation() {
+      log.info("프록시 호출");
+      if(cacheValue==null){     //cacheValue에 값이 있으면 RealSubject 호출
+          cacheValue = target.operation();  //첫번재 호출일때는 null  떄문에 RealSubject 호출
+      } //프록시가 호출한 대상 : target
+        return cacheValue;      //두번째 호출일때는 null 이 아니니까 cacheValue 로 호출
+
+    }
+}
