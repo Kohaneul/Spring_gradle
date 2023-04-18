d5b733ea44934dc229da31407747910e795d9317
Kohaneul <happyhn_@naver.com>
2023-04-18 21:48:44 +0900

    0417 commit

--- 6. spring advanced/proxy/src/main/java/hello/proxy/config/v5_autoproxy/AutoProxyConfig.java
+++ 6. spring advanced/proxy/src/main/java/hello/proxy/config/v5_autoproxy/AutoProxyConfig.java
@@ -5,6 +5,7 @@ import hello.proxy.config.AppV2Config;
 import hello.proxy.config.v3_proxyfactory.advice.LogTraceAdvice;
 import hello.proxy.trace.logtrace.LogTrace;
 import org.springframework.aop.Advisor;
+import org.springframework.aop.aspectj.AspectJExpressionPointcut;
 import org.springframework.aop.support.DefaultPointcutAdvisor;
 import org.springframework.aop.support.NameMatchMethodPointcut;
 import org.springframework.context.annotation.Bean;
@@ -14,10 +15,30 @@ import org.springframework.context.annotation.Import;
 @Configuration
 @Import({AppV1Config.class, AppV2Config.class})
 public class AutoProxyConfig {
-    @Bean   //BeanPostProcessor 대신 Advisor만 bean으로 등록하면 된다.
+    //@Bean   //BeanPostProcessor 대신 Advisor만 bean으로 등록하면 된다.
     public Advisor advisor1(LogTrace logTrace){
         NameMatchMethodPointcut pointcut = new NameMatchMethodPointcut();
         pointcut.setMappedNames("request*","save*","order*");
+        LogTraceAdvice advice = new LogTraceAdvice(logTrace);
+        return new DefaultPointcutAdvisor(pointcut,advice);
+    }
+
+
+ //   @Bean
+    public Advisor advisor2(LogTrace logTrace){
+        //pointcut
+        AspectJExpressionPointcut pointcut = new AspectJExpressionPointcut();
+        pointcut.setExpression("execution(* hello.proxy.app..*(..))");  //hello.proxy.app 하위 모든 메서드는 포인트컷 매칭 대상이 된다.
+        //advice
+        LogTraceAdvice advice = new LogTraceAdvice(logTrace);
+        return new DefaultPointcutAdvisor(pointcut,advice);
+    }
+    @Bean
+    public Advisor advisor3(LogTrace logTrace){
+        //pointcut
+        AspectJExpressionPointcut pointcut = new AspectJExpressionPointcut();
+        pointcut.setExpression("execution(* hello.proxy.app..*(..)) && !execution(* hello.proxy.app..noLog(..))");
+        //advise
         LogTraceAdvice advice = new LogTraceAdvice(logTrace);
         return new DefaultPointcutAdvisor(pointcut,advice);
     }
