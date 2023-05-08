8dd0c87e3743aa7c66598a214977ef2b0085fd58
Kohaneul <happyhn_@naver.com>
2023-05-08 21:53:00 +0900

    0508 commit

--- 6. spring advanced/aop/src/test/java/hello/aop/pointcut/ExecutionTest.java
+++ 6. spring advanced/aop/src/test/java/hello/aop/pointcut/ExecutionTest.java
@@ -5,7 +5,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.aop.aspectj.AspectJExpressionPointcut;
-import org.springframework.boot.test.context.SpringBootTest;
 
 import java.lang.reflect.Method;
 
