cbddfab4a461f553bea11b68515ce4acbe35e2a1
Kohaneul <happyhn_@naver.com>
2023-05-17 21:27:07 +0900

    0517 commit

--- 6. spring advanced/aop/src/test/java/hello/aop/pointcut/ExecutionTest.java
+++ 6. spring advanced/aop/src/test/java/hello/aop/pointcut/ExecutionTest.java
@@ -7,12 +7,15 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.aop.aspectj.AspectJExpressionPointcut;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import java.lang.reflect.Method;
 
+import static org.assertj.core.api.Assertions.as;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @Slf4j
+@SpringBootTest
 public class ExecutionTest {
    AspectJExpressionPointcut pointcut = new AspectJExpressionPointcut();
    Method helloMethod;
@@ -74,7 +77,7 @@ public class ExecutionTest {
     @Test
     void packageExactMatchFalse(){
        pointcut.setExpression("execution(* hello.aop.*.*(..))");    //hello.aop 패키지에 맞아야함
-       assertThat(pointcut.matches(helloMethod,MemberServiceImpl.class)).isTrue();
+       assertThat(pointcut.matches(helloMethod,MemberServiceImpl.class)).isFalse();
     }
 
     @Test
@@ -88,5 +91,74 @@ public class ExecutionTest {
        pointcut.setExpression("execution(* hello.aop..*.*(..))");
        assertThat(pointcut.matches(helloMethod,MemberServiceImpl.class)).isTrue();
     }
+
+
+    @Test
+    void typeExactMatch(){
+       pointcut.setExpression("execution(* hello.aop.member.MemberServiceImpl.*(..))");
+       assertThat(pointcut.matches(helloMethod,MemberServiceImpl.class)).isTrue();
+    }
+
+
+    @Test
+    void typeExactSuperType(){
+       pointcut.setExpression("execution(* hello.aop.member.MemberService.*(..))");
+       //MemberService =>MemberServiceImpl 의 부모타입으로 매칭시에도 성공
+       assertThat(pointcut.matches(helloMethod,MemberServiceImpl.class)).isTrue();
+    }
+    @Test
+    void typeMatchInternal() throws NoSuchMethodException {
+       pointcut.setExpression("execution(* hello.aop.member.MemberServiceImpl.*(..))");
+        Method internalMethod = MemberServiceImpl.class.getMethod("internal", String.class);
+        assertThat(pointcut.matches(internalMethod,MemberServiceImpl.class)).isTrue();
+    }
+
+
+    @Test
+    void typeMatchInternalNoSuperTypeMethodFalse() throws NoSuchMethodException {
+       pointcut.setExpression("execution(* hello.aop.member.MemberService.*(..))");
+        Method internalMethod = MemberServiceImpl.class.getMethod("internal", String.class);
+        //internal => 자식타입에 있는 다른 메서드까지 매칭이 되는것인가..??
+        assertThat(pointcut.matches(internalMethod,MemberServiceImpl.class)).isFalse();
+        //부모타입(인터페이스)에 선언된 메서드까지 매칭
+   }
+
+   //String 타입의 파라미터 허용
+   @Test
+    void argsMatch(){
+       pointcut.setExpression("execution(* *(String))");
+       assertThat(pointcut.matches(helloMethod,MemberServiceImpl.class)).isTrue();
+   }
+   //파라미터가 없어야 함
+   @Test
+    void argsMatchNoArgs(){
+       pointcut.setExpression("execution(* *())");
+       assertThat(pointcut.matches(helloMethod,MemberServiceImpl.class)).isFalse();
+   }
+
+   //정확히 하나의 파라미터 허용, 모든 타입 허용
+   @Test
+    void argsMatchStar(){
+       pointcut.setExpression("execution(* *(*))");
+       assertThat(pointcut.matches(helloMethod,MemberServiceImpl.class)).isTrue();
+   }
+
+   //파라미터 갯수와 무관하게 모든 파라미터, 모든 타입 허용
+    //() (Xxx), (Xxx,xx)
+   @Test
+    void argsMatchAll(){
+       pointcut.setExpression("execution(* *(..))");
+       assertThat(pointcut.matches(helloMethod,MemberServiceImpl.class)).isTrue();
+   }
+
+    //String 타입으로 시작 ,파라미터 갯수와 무관하게 모든 파라미터, 모든 타입 허용
+    //(String), (String, Xxx), (String, Xxx,xx)
+   @Test
+    void argsMatchComplex(){
+       pointcut.setExpression("execution(* *(String, ..))");
+       // 파라미터 String, String -> (String,String)
+       //파라미터 두개인데 처음엔 무조건 String -> (String, *)
+       assertThat(pointcut.matches(helloMethod,MemberServiceImpl.class)).isTrue();
+   }
 
 }
